289bdbc9df7d6ea5ee62d4fb6675d51525840be1
srm14 <srm14@illinois.edu>
2021-07-21 00:03:04 -0700

    Changed default model location.

--- process_mic_input.py
+++ process_mic_input.py
@@ -49,7 +49,7 @@ args = parser.parse_args(remaining)
 
 try:
     if args.model is None:
-        args.model = "model"
+        args.model = "data/model" # Add model folder location here
     if not os.path.exists(args.model):
         print ("Please download a model for your language from https://alphacephei.com/vosk/models")
         print ("and unpack as 'model' in the current folder.")
