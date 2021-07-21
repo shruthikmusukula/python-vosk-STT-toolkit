7ba5f9a166d58d72b0b9bb369a78b315a7fcd75a
srm14 <srm14@illinois.edu>
2021-07-21 00:02:29 -0700

    Added srt construction functionality to extractor class.

--- extractor.py
+++ extractor.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
 
 from vosk import Model, KaldiRecognizer, SetLogLevel
-import os
+
+import datetime
 import json
+import os
+import srt
 import subprocess
 
-SAMPLE_RATE = 16000
 MODEL_PATH = "data/model"
+SAMPLE_RATE = 16000
+WORDS_PER_LINE = 7
 
 class VOSK_Extractor:
     def __init__(self, file, sampleRate=None, model=None):
+        self.check_for_model()
+
         self.dataFile = file
 
         if sampleRate is None:
@@ -22,10 +28,16 @@ class VOSK_Extractor:
         else:
             self.model = Model(model)
 
+        self.output = ""
+
         # VOSK General Model Setup
         SetLogLevel(0)
-        self.check_for_model()
         self.rec = KaldiRecognizer(self.model, self.sampleRate)
+        self.rec.SetWords(True)
+
+        # Initialize SRT Variables
+        self.results = []
+        self.subs = []
 
     def check_for_model(self):
         if not os.path.exists("data/model"):
@@ -42,10 +54,10 @@ class VOSK_Extractor:
 
     def prepare_result(self):
         # Load string into JSON format and extract resulting text
-        result = json.loads(self.rec.FinalResult())
-        print("Result from [" + self.dataFile + "]: " + result["text"])
+        output = json.loads(self.output)
+        print("Result from [" + self.dataFile + "]: " + output["text"] + "\n")
 
-    def process_ffmpeg(self):
+    def process_input_file(self):
         # Call to initialize subprocess with VOSK API
         self.initialize_process()
 
@@ -55,9 +67,31 @@ class VOSK_Extractor:
             if len(data) == 0:
                 break
             if self.rec.AcceptWaveform(data):
-                self.rec.Result()
+                self.results.append(self.rec.Result())
+
+        # Save extracted output
+        self.output = self.rec.FinalResult()
+
+        # Add extracted text to results
+        self.results.append(self.output)
 
         # Call to prepare result
         self.prepare_result()
+
+    def built_srt(self):
+        # Build SRT File content
+        for i, res in enumerate(self.results):
+            jres = json.loads(res)
+            if not 'result' in jres:
+                continue
+            words = jres['result']
+            for j in range(0, len(words), WORDS_PER_LINE):
+                line = words[j: j + WORDS_PER_LINE]
+                s = srt.Subtitle(index=len(self.subs),
+                                 content=" ".join([l['word'] for l in line]),
+                                 start=datetime.timedelta(seconds=line[0]['start']),
+                                 end=datetime.timedelta(seconds=line[-1]['end']))
+                self.subs.append(s)
+        print(srt.compose(self.subs))
 
 # Lone Executable Command (If Main Was In Here): python3 extractor.py data/test.mp4
