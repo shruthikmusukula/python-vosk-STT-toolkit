17af11b9a0600036e2572988363910e8f3866016
srm14 <srm14@illinois.edu>
2021-07-20 19:32:07 -0700

    Set model, sampleRate, and input video file parameters.

--- process_ffmpeg.py
+++ process_ffmpeg.py
@@ -3,15 +3,19 @@
 from vosk import Model, KaldiRecognizer, SetLogLevel
 import sys
 import os
-import wave
+import json
 import subprocess
 
+SAMPLE_RATE = 16000
+
 class VOSK_Extractor:
-    def __init__(self, SAMPLE_RATE=None, model=None):
-        if SAMPLE_RATE is None:
-            self.SAMPLE_RATE = 16000
+    def __init__(self, file, sampleRate=None, model=None):
+        self.dataFile = file
+
+        if sampleRate is None:
+            self.sampleRate = SAMPLE_RATE
         else:
-            self.SAMPLE_RATE = SAMPLE_RATE
+            self.sampleRate = sampleRate
 
         if model is None:
             self.model = Model("data/model")
@@ -21,33 +25,39 @@ class VOSK_Extractor:
         # VOSK Model Setup
         SetLogLevel(0)
         self.check_for_model()
-        self.rec = KaldiRecognizer(self.model, self.SAMPLE_RATE)
+        self.rec = KaldiRecognizer(self.model, self.sampleRate)
 
     def check_for_model(self):
         if not os.path.exists("data/model"):
             print(
-                "Please download the model from https://alphacephei.com/vosk/models and unpack as 'model' in the current folder."
+                "Please download the model from https://alphacephei.com/vosk/models and unpack as 'test_name' in your working directory."
                 "This is the training model needed to work with the VOSK API.")
             exit(1)
 
     def process_ffmpeg(self):
-        process = subprocess.Popen(['ffmpeg', '-loglevel', 'quiet', '-i',
-                                    sys.argv[1],
-                                    '-ar', str(self.SAMPLE_RATE) , '-ac', '1', '-f', 's16le', '-'],
+        process = subprocess.Popen(['ffmpeg', '-loglevel', 'quiet', '-i', self.dataFile, '-ar',
+                                    str(self.sampleRate) , '-ac', '1', '-f', 's16le', '-'],
                                     stdout=subprocess.PIPE)
         while True:
             data = process.stdout.read(4000)
             if len(data) == 0:
                 break
             if self.rec.AcceptWaveform(data):
-                print(self.rec.Result())
-            else:
-                print(self.rec.PartialResult())
-        print(self.rec.FinalResult())
+                self.rec.Result()
+                # print(self.rec.Result())
+            # else:
+            #     print(self.rec.PartialResult())
+
+    def prepare_result(self):
+        # Load string into JSON format and extract resulting text
+        result = json.loads(self.rec.FinalResult())
+        print("Result from [" + self.dataFile + "]: " + result["text"])
+
 
 
 if __name__ == "__main__":
-    extractor = VOSK_Extractor()
+    extractor = VOSK_Extractor(sys.argv[1])
     extractor.process_ffmpeg()
+    extractor.prepare_result()
 
-# Executable: python3 process_ffmpeg.py data/test.mp4
+# Executable Command: python3 process_ffmpeg.py data/test.mp4
